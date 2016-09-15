8235e6020e811efb34d5f1c74353def75037e9ab
marlodam <marlodam@greeny.cs.tlu.ee>
2016-09-15 16:21:31 +0300

    täiendasin

--- login.php
+++ login.php
@@ -4,10 +4,53 @@
 	//echo "<br>";
 	//var_dump($_POST);
 	
+		$loginEmailError = "";
+		$loginPasswordError = "";
+	
+	// kas epost oli olemas
+	if (isset ($_POST["loginEmail"])) {
+		
+		if (empty ($_POST["loginEmail"])) {
+			
+			// oli email, kuid see oli tühi
+			$loginEmailError = "See väli on kohustuslik!";
+			
+		}
+		
+	}
+	
+	
+	if (isset ($_POST["loginPassword"])) {
+		
+		if (empty ($_POST["loginPassword"])) {
+			
+			// oli password, kuid see oli tühi
+			$loginPasswordError = "See väli on kohustuslik!";
+			
+		} else {
+			
+			// tean et parool on ja see ei olnud tühi
+			// VÄHEMALT 8 tähemärki peab parool olema
+			
+			if (strlen($_POST["loginPassword"]) < 8 ) {
+				
+				$loginPasswordError = "Parool peab olema vähemalt 8 tähemärkki pikk";
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	
+	
 	$signupEmailError = "";
 	$signupPasswordError = "";
+	$signupYearOfBirthError = "";
+	$signupSexError = "";
 	
-	// kas e/post oli olemas
+	// kas epost oli olemas
 	if (isset ($_POST["signupEmail"])) {
 		
 		if (empty ($_POST["signupEmail"])) {
@@ -41,6 +84,62 @@
 		
 	}
 	
+	
+	
+	
+	
+		if (isset ($_POST["signupYearOfBirth"])) {
+		
+		if (empty ($_POST["signupYearOfBirth"])) {
+			
+			// oli sünniaasta, kuid see oli tühi
+			$signupYearOfBirthError = "See väli on kohustuslik!";
+			
+		} else {
+			
+			// tean et sünniaasta on ja see ei olnud tühi
+			// VÄHEMALT 4 tähemärki peab sünniaasta pikk olema
+			
+			if (strlen($_POST["signupYearOfBirth"]) < 4 ) {
+				
+				$signupYearOfBirthError = "Sünniaasta peab olema vähemalt 4 tähemärkki pikk";
+				
+			}
+			
+		}
+		
+	}
+	
+	
+	
+		if (isset ($_POST["signupSex"])) {
+		
+		if (empty ($_POST["signupSex"])) {
+			
+			// oli sugu, kuid see oli tühi
+			$signupSexError = "See väli on kohustuslik!";
+			
+		} else {
+			
+			// tean et sugu on ja see ei olnud tühi
+			// VÄHEMALT 4 tähemärki peab sünniaasta pikk olema
+			
+			if (strlen($_POST["signupSex"]) < 4 ) {
+				
+				$signupSexError = "Sugu peab olema vähemalt 4 tähemärkki pikk";
+				
+			}
+			
+		}
+		
+	}
+	
+	
+
+	
+	
+	
+		
 
 ?>
 <!DOCTYPE html>
@@ -55,11 +154,12 @@
 		<form method="POST">
 			
 			<label>E-post</label><br>
-			<input name="loginEmail" type="email">
+			<input name="loginEmail" type="email"> <?php echo $loginEmailError; ?>
 			
 			<br><br>
 			
-			<input name="loginPassword" type="password" placeholder="Parool">
+			<label>Parool</label><br>
+			<input name="loginPassword" type="password"> <?php echo $loginPasswordError; ?>
 			
 			<br><br>
 			
@@ -71,14 +171,26 @@
 		
 		<form method="POST">
 			
-			<label>E-post</label><br>
-			<input name="signupEmail" type="email"> <?php echo $signupEmailError; ?>
+			<input name="signupEmail" type="email" placeholder="E-post"> <?php echo $signupEmailError; ?>
 			
 			<br><br>
+			
 			
 			<input name="signupPassword" type="password" placeholder="Parool"> <?php echo $signupPasswordError; ?>
 			
 			<br><br>
+			
+			
+			
+			<input name="signupYearOfBirth" type="YearOfBirth" placeholder="Sünniaasta"> <?php echo $signupYearOfBirthError; ?>
+			
+			<br><br>
+			
+			
+			<input name="signupSex" type="Sex" placeholder="Sugu"> <?php echo $signupSexError; ?>
+			
+			<br><br>
+			
 			
 			<input type="submit" value="Loo kasutaja">
 			
